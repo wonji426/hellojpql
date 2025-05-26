ae0a83ae5433f65fb0d4dac61620468aea04b595
jiwon <kjw042626@gmail.com>
2025-05-26 20:22:25 +0900

    project setup 1.1

---
+++ src/main/java/jpql/JpaMain.java
@@ -0,0 +1,39 @@
+package jpql;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.EntityTransaction;
+import jakarta.persistence.Persistence;
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Root;
+
+import java.util.List;
+
+public class JpaMain {
+
+    public static void main(String[] args) {
+
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction tx = em.getTransaction();
+
+        tx.begin();
+
+        try {
+
+            Member member = new Member();
+            member.setUsername("member1");
+            em.persist(member);
+
+
+            tx.commit(); //커밋시 SQL문 나감
+        } catch (Exception e) {
+            tx.rollback();//에러 시 롤백
+        } finally {
+            em.close();//닫기
+        }
+
+        emf.close();
+    }
+}
