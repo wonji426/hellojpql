d45e1be91c6b2c2748ca3801dce57b33d0a415cd
jiwon <kjw042626@gmail.com>
2025-05-27 17:58:30 +0900

    기본 문법과 쿼리 API

--- src/main/java/jpql/JpaMain.java
+++ src/main/java/jpql/JpaMain.java
@@ -1,9 +1,6 @@
 package jpql;
 
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.EntityManagerFactory;
-import jakarta.persistence.EntityTransaction;
-import jakarta.persistence.Persistence;
+import jakarta.persistence.*;
 import jakarta.persistence.criteria.CriteriaBuilder;
 import jakarta.persistence.criteria.CriteriaQuery;
 import jakarta.persistence.criteria.Root;
@@ -24,8 +21,26 @@ public class JpaMain {
 
             Member member = new Member();
             member.setUsername("member1");
+            member.setAge(10);
             em.persist(member);
 
+            TypedQuery<Member> query1 = em.createQuery("select m from Member m", Member.class);
+            TypedQuery<String> query2 = em.createQuery("select m.username from Member m", String.class);
+            Query query3 = em.createQuery("select m.username, m.age from Member m");
+
+            List<Member> resultList = query1.getResultList();
+            for (Member member1 : resultList) {
+                System.out.println("member1 = " + member1);
+            }
+
+            TypedQuery<Member> query4 = em.createQuery("select m from Member m where m.id = 1", Member.class);
+            Member singleResult = query4.getSingleResult();
+            System.out.println("singleResult = " + singleResult);
+
+            TypedQuery<Member> query5 = em.createQuery("select m from Member m where m.username = :username", Member.class);
+            query5.setParameter("username", "member1");
+            Member singleResult1 = query5.getSingleResult();
+            System.out.println("singleResult1 = " + singleResult1.getUsername());
 
             tx.commit(); //커밋시 SQL문 나감
         } catch (Exception e) {
